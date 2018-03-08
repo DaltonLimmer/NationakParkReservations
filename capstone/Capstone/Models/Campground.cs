36a03837ccdd03a54d6e9aa8e80ed74afc3b47d6
Dalton Limmer <dlimmer.10@gmail.com>
2018-03-08 11:06:40 -0500

    DALs

---
+++ capstone/Capstone/Models/Campground.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    class Campground
+    {
+        public int Campground_Id { get; set; }
+        public int Park_Id { get; set; }
+        public string Name { get; set; }
+        public int Open_From_MM { get; set; }
+        public int Open_To_MM { get; set; }
+        public double Daily_Fee { get; set; }
+
+    }
+}
