679edda00127885c4ba3e649fdab28bf6d92acad
Byron Thompson <fullstackbyron@gmail.com>
2018-03-08 11:14:04 -0500

    Working on CLI

---
+++ capstone/Capstone/ProgramCLI.cs
@@ -0,0 +1,136 @@
+﻿using Capstone.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone
+{
+    public class ProgramCLI
+    {
+        private const string command_Cancel = "0";
+        private const string command_SelectAcadia = "1";
+        private const string command_SelectArches = "2";
+        private const string command_SelectCuyahoga = "3";
+        private const string command_Quit = "q";
+        private const string command_ViewCampgrounds = "1";
+        private const string command_SearchReservations = "2";
+        private const string command_ReturnToPreviousScreen = "3";
+        private const string command_SearchAvailableReservatgions = "1";
+        private const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Campground;Integrated Security = True";
+        private Dictionary<int, string> Parks = new Dictionary<int, string>()
+        {
+            {1, "Acadia" },
+            {2, "Arches" },
+            {3, "Cuyahoga National Valley" }
+        };
+        
+
+
+
+        public void RunCLI()
+        {
+            bool stillRunning = true;
+
+            while (stillRunning)
+            {
+                ConsoleKeyInfo userInput = Console.ReadKey();
+
+                switch (userInput.ToString())
+                {
+                    case command_SelectAcadia:
+
+                        break;
+                    case command_SelectArches:
+                        break;
+                    case command_SelectCuyahoga:
+                        break;
+                    case command_Quit:
+                        return;
+                    default:
+                        Console.WriteLine("The command provided was not a valid command, please try again.");
+                        break;
+                }
+            }
+        }
+
+        private void PrintMainMenu()
+        {
+            Console.WriteLine("Select a Park for further Details");
+            Console.WriteLine(" 1) Acadia");
+            Console.WriteLine(" 2) Arches");
+            Console.WriteLine(" 3) Cuyahoga NAtional Valley Park");
+            Console.WriteLine();
+
+            Console.WriteLine(" Q) Quit");
+            Console.WriteLine();
+
+        }
+
+        private void PrintParkInfoMenu()
+        {
+            Console.WriteLine("Select a Park for further Details");
+            Console.WriteLine(" 1) View Campgrounds");
+            Console.WriteLine(" 2) Search for Reservations");
+            Console.WriteLine(" 3) Return to Previous Screen");
+            Console.WriteLine();
+
+        }
+
+        private void PrintCampgroundMenu()
+        {
+            Console.WriteLine("Select a Park for further Details");
+            Console.WriteLine(" 1) Search for Available Reservation");
+            Console.WriteLine(" 2) Return to Previous Screen");
+            Console.WriteLine();
+
+        }
+
+        // Search
+        private void GetAllParks()
+        {
+
+            CampgroundSqlDAL campgroundDAL = new CampgroundSqlDAL();
+
+        }
+
+        private void GetParkWithCampgrounds()
+        {
+            CampgroundSqlDAL campgroundDAL = new CampgroundSqlDAL();
+
+        }
+
+        //Availabilities
+        private void GetParkInfo()
+        {
+
+        }
+        private void GetParkWithAvailableDates()
+        {
+
+        }
+
+        private void GetParkCampsiteAdvancedSearch()
+        {
+
+        }
+
+        private void SearchParkForAvailability()
+        {
+
+        }
+
+        //Booking
+        private void BookReservation()
+        {
+
+        }
+
+        private void SearchParkForMadeReservations()
+        {
+
+        }
+
+    }
+}
---
+++ m2-w6d1-dao-exercises-pair/ProjectDB.Tests/DAL/ProjectSqlDALTestcs.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using ProjectDB.DAL;
+using System.Data.SqlClient;
+using ProjectDB.Models;
+
+namespace ProjectDB.Tests.DAL
+{
+    [TestClass]
+    public class ProjectSqlDALTests
+    {
+        private TransactionScope tran;
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=ProjectOrganizer2;Integrated Security = True";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd;
+                conn.Open();
+
+                //Total Number of projects
+                cmd = new SqlCommand("Select COUNT(*) From Project", conn);
+                cmd.ExecuteReader();
+
+            }
+        }
+
+        // Cleanup Rollback
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+        [TestMethod]
+        public void GetProjectsTest()
+        {
+            //Arrange
+            ProjectSqlDAL projectDAL = new ProjectSqlDAL(connectionString);
+
+            //Act
+            List<Project> projects = projectDAL.GetAllProjects();
+
+            //Assert
+            Assert.AreEqual(7, projects.Count);
+        }
+
+        //Assign
+        [TestMethod]
+        public void AssignEmployeeToProjectTest()
+        {
+            //Arrange
+            ProjectSqlDAL projectDAL = new ProjectSqlDAL(connectionString);
+
+            //Act
+            bool wasSuccesful = projectDAL.AssignEmployeeToProject(2, 5);
+
+            //Assert
+            Assert.AreEqual(true, wasSuccesful);
+        }
+
+        //Remove
+        [TestMethod]
+        public void RemoveEmployeeFromProjectTest()
+        {
+            //Arrange
+            ProjectSqlDAL projectDAL = new ProjectSqlDAL(connectionString);
+
+            //Act
+            bool wasSuccesful = projectDAL.RemoveEmployeeFromProject(1, 3);
+
+            //Assert
+            Assert.AreEqual(true, wasSuccesful);
+        }
+
+        //Create
+        [TestMethod]
+        public void CreateProjectTest()
+        {
+            //Arrange
+            ProjectSqlDAL projectDAL = new ProjectSqlDAL(connectionString);
+            Project newProj = new Project()
+            {
+                Name = "TechElevator",
+                StartDate = new DateTime(2018,01,29),
+                EndDate = new DateTime(2018, 05, 04)
+            };
+
+
+            //Act
+            bool wasSuccessful = projectDAL.CreateProject(newProj);
+
+            //Assert
+            Assert.AreEqual(true, wasSuccessful);
+        }
+    }   
+}
