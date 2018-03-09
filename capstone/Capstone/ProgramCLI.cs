bc28d2398ef1afd984c23565f6f1ca146291d98d
Byron Thompson <fullstackbyron@gmail.com>
2018-03-09 09:13:19 -0500

    Added Months Dictionary in PRogramCLI

--- capstone/Capstone/ProgramCLI.cs
+++ capstone/Capstone/ProgramCLI.cs
@@ -27,6 +27,21 @@ namespace Capstone
             {2, "Arches" },
             {3, "Cuyahoga Valley" }
         };
+        private Dictionary<int, string> Months = new Dictionary<int, string>()
+        {
+            {1,"January" },
+            {2, "February" },
+            {3,"March" },
+            {4, "April" },
+            {5, "May" },
+            {6, "June" },
+            {7, "July" },
+            {8, "August" },
+            {9, "September" },
+            {10, "October" },
+            {11, "November" },
+            {12, "December" }
+        };
         
 
 
